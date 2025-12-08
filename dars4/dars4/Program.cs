12349571745da122a3ee2e89cdf0dceae18cc63e
abdusamad2009 <abduqaxxorov2009@gmail.com>
2025-12-08 14:49:16 +0500

    dars4

---
+++ dars4/dars4/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace dars4
+{
+    internal class Program
+    {
+
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+
+            //    for (int i = 100;i < 1000; i++)
+            //    {
+            //        int r1 = i % 100;
+            //        int r2 = i / 10 % 10;
+            //        int r3 = i % 10;
+
+            //        if (r1 % 2 != 0 && r2 % 2 != 0 && r3 % 2 != 0)
+            //        {
+            //            Console.Write($"{i} ,");
+            //        }
+            //    }
+
+
+
+
+
+            //string text = "davay";
+
+            //var res2 = text.Substring(,3);
+
+            //Console.WriteLine(res2);
+        }
+    }
+}
