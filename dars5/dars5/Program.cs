a087bf78fad026a8a72909fdde339fd7783cdf66
abdusamad2009 <abduqaxxorov2009@gmail.com>
2025-12-09 17:21:04 +0500

    dars5

---
+++ dars5/dars5/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace dars5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("list uzunligini kiriting : ");
+            var n = int.Parse(Console.ReadLine());
+
+            List<int> ints = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("listni elementini kiriting : ");
+                var num = int.Parse(Console.ReadLine());
+                ints.Add(num);
+            }
+        }
+    }
+}
