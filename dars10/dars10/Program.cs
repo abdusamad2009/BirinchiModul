e787ea14f9cd6b53c3e1b23f07122a42b8b6650f
abdusamad2009 <abduqaxxorov2009@gmail.com>
2025-12-18 17:40:43 +0500

    dars10

---
+++ dars10/dars10/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace dars10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone phone1 = new Phone()
+            {
+                Color = "Blue",
+                Model = "S20 ultra",
+                Brand = "Samsung",
+                ManifacturedYrar = 2023,
+                Storage = 1024,
+
+
+
+            };
+        }
+    }
+
+}
