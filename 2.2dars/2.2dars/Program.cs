71bf31b334313bee952a79bcb3d7a4c4795b3efc
abdusamad2009 <abduqaxxorov2009@gmail.com>
2026-01-06 15:55:46 +0500

    2.2dars

---
+++ 2.2dars/2.2dars/Program.cs
@@ -0,0 +1,91 @@
+﻿
+
+namespace _2._2dars;
+
+
+internal class Program
+{
+    static List<Book> Books = new List<Book>();
+    static void Main(string[] args)
+    {
+        DataSeed();
+
+        var books = GetExpensiveBooksThenMinPrice(5);
+
+
+
+        //var book = GetMaxPricedBook();
+        //Console.WriteLine(book.Price);
+
+    }
+
+    static List<Book> GetExpensiveBooksThenMinPrice(decimal minPrice)
+    {
+        var expensiveBooks = new List<Book>();
+
+        foreach (var book in Books)
+        {
+            if (book.Price > minPrice)
+            {
+                expensiveBooks.Add(book);
+            }
+        }
+
+        return expensiveBooks;
+    }
+
+    static Book GetMaxPricedBook()
+    {
+        Book book = Books[0];
+
+        foreach (var b in Books)
+        {
+            if (book.Price < b.Price)
+            {
+                book = b;
+            }
+        }
+
+        return book;
+    }
+
+
+    static void DataSeed()
+    {
+        Books.Add(new Book()
+        {
+            BookId = Guid.NewGuid(),
+            Title = "The Great Gatsby",
+            Author = "F. Scott Fitzgerald",
+            Price = 10.99m,
+            Description = "A novel set in the Jazz Age that tells the story of Jay Gatsby's unrequited love for Daisy Buchanan.",
+            Created = DateTime.Now,
+            Genre = "Classic",
+            PageCount = 180
+        });
+
+        Books.Add(new Book()
+        {
+            BookId = Guid.NewGuid(),
+            Title = "Sariq Devni Minib",
+            Author = "Xudoyberdi To'xtaboyev",
+            Price = 5.99m,
+            Description = "Legenda",
+            Created = DateTime.Now,
+            Genre = "Fantasy",
+            PageCount = 270
+        });
+
+        Books.Add(new Book()
+        {
+            BookId = Guid.NewGuid(),
+            Title = "Harry Poter",
+            Author = "Opa",
+            Price = 15.99m,
+            Description = "Legenda 2",
+            Created = DateTime.Now,
+            Genre = "Fantasy",
+            PageCount = 870
+        });
+    }
+}
